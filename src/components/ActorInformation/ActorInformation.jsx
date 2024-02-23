3c50c8e0f6e3cca709af83a6ace8bc4e9431d784
AksLIntellipat <118931093+akslintellipat@users.noreply.github.com>
2024-02-23 23:03:58 +0530

    routes and component structure

---
+++ src/components/ActorInformation/ActorInformation.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ActorInformation = () => {
+  console.log(' ');
+  return <div>ActorInformation</div>;
+};
+
+export default ActorInformation;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+import MovieInformation from './MovieInformation/MovieInformation';
+import ActorInformation from './ActorInformation/ActorInformation';
+import Profile from './Profile/Profile';
+import Movies from './Movies/Movies';
+import NavBar from './NavBar/NavBar';
 
-const App = () => <div>App</div>;
+const App = () => (
+  <div>
+    <CssBaseline />
+    <NavBar />
+    <main>
+      <Switch>
+        <Route exact path="/movie/:id">
+          <MovieInformation />
+        </Route>
+        <Route exact path="/actor/:id">
+          <ActorInformation />
+        </Route>
+        <Route exact path="/profile/:id">
+          <Profile />
+        </Route>
+        <Route exact path="/">
+          <Movies />
+        </Route>
+      </Switch>
+    </main>
+  </div>
+);
 
 export default App;
